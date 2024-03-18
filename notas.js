f59cb0c8d1bdd33fd6d626778a48b0bf6cfe0d11
Clarissa Modesto <141753467+clarissamodesto@users.noreply.github.com>
2024-03-18 10:42:20 -0300

    Add files via upload

---
+++ notas.js
@@ -0,0 +1,21 @@
+
+//Variaveis
+let nota1 = 8;
+let nota2 = 7;
+let nota3 = 7;
+
+let media = (nota1 + nota2 + nota3) / 3;
+
+console.log(media);
+
+if (media < 5) {
+    console.log ("reprovado");
+} else if (media >= 5 >= 7) {
+    console.log ("recuperação");
+} else
+    console.log ("aprovado");
+
+
+
+
+
